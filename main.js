8b4bc5ceb684cdf905b542d44f1339785474180c
RaxHRB <bodolicarazvan24@gmail.com>
2023-04-11 14:16:16 +0300

    Last finishes

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 var newName = document.getElementById("city-input");
 var cityName = document.getElementById("city-name");
 var backgroundAnimated = document.getElementById("bd");
-var idSavedLocation;
-var d = new Date();
 var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let favorite=[];
+var d = new Date();
+var idSavedLocation;
 var store;
+var date;
+var hours;
+var minutes
+var formattedTime
+let favorite=[];
+let unix_timestamp;
 
-//---------Saving searches--------------
+//----Send data when enter key is pressed---------
+newName.onkeydown = function(e){
+    if(e.keyCode == 13){
+      return getApi();
+    }
+ };
 
+//-----Get data form local storage----------------
 window.onload = function(){
     store = JSON.parse(localStorage.getItem('stored-items'));
     if(store != null){
@@ -22,10 +33,14 @@ window.onload = function(){
     }
 }
 
+//----Save data to local storage + conditions---------
 function saveLocation(){
     if(favorite.length > 2){
         alert("No more space: delete saved location");
     }else{
+        if(newName.value == ''){
+            alert("The input is blank");
+        }else{
         favorite.push(newName.value);
         localStorage.setItem('stored-items', JSON.stringify(favorite));
         store = JSON.parse(localStorage.getItem('stored-items'));
@@ -37,14 +52,17 @@ function saveLocation(){
             if(document.getElementById("saved-location" + (i+1)).innerHTML != 'undefined'){
                 document.getElementById("saved-location"  + (i+1)).style.visibility = 'visible';
                 }
+            }
         }
     }
 }
 
+//----Get id from saved location buttons---------
 function getId(clicked_id){
     idSavedLocation = clicked_id;
 }
 
+//----Get location and send to getApi function---------
 function getLocation(){
     if(idSavedLocation == "saved-location1"){
         newName.value = document.getElementById("saved-location1").innerHTML; 
@@ -56,10 +74,12 @@ function getLocation(){
     return getApi();
 }
 
+//----Clear search input---------
 function clearInput(){
     newName.value='';
 }
 
+//----Clear saved location and localstorage---------
 function clearSavedLocation(){
     favorite = [];
     store =[];
@@ -70,6 +90,7 @@ function clearSavedLocation(){
     }
 }
 
+//----Get the next days from today---------
 function checkDay(day){
     if(day + d.getDay() > 6){
         return day + d.getDay() - 7;
@@ -83,13 +104,7 @@ function checkDay(day){
     document.getElementById("day" + (i+1)).innerHTML = weekday[checkDay(i)];
 }
 
-
-let unix_timestamp;
-var date;
-var hours;
-var minutes
-var formattedTime
-
+//----Transform dt into time(ex.20:00)---------
 function getTime(x){
     unix_timestamp = x;
     date = new Date(unix_timestamp * 1000);
@@ -99,6 +114,7 @@ function getTime(x){
     return formattedTime;
 }
 
+//----Get only hour from dt---------
 function getHour(x){
     unix_timestamp = x;
     date = new Date(unix_timestamp * 1000);
@@ -107,7 +123,7 @@ function getHour(x){
     return formattedTime;
 }
 
-
+//----Get weather data from openweather api---------
 function getApi(){
  
 fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=909cb0df41b4da1d8c55cb206b7d0a1f')  
@@ -118,16 +134,23 @@ var sunriseDt = data.city.sunrise;
 var sunsetDt = data.city.sunset
 var sunriseHour = getTime(sunriseDt);
 var sunsetHour = getTime(sunsetDt);
+var currentIcon = data.list[0].weather[0].main;
+var currentDt = data.list[0].dt;
+var currentHour = getHour(currentDt);
+let k=8;
+
 
 cityName.innerHTML = newName.value;
 newName.value ='';
 
+//----Get the next hours---------
 for(i = 1; i<6; i++){
     var dt = data.list[i].dt;
     var timeHours = getTime(dt);
     document.getElementById("hour" + (i+1)).innerHTML = timeHours;
 }
 
+//----Get data for the main card---------
 document.getElementById("temperature-day1").innerHTML = Number(data.list[0].main.temp -273.15).toFixed(0) + "°";
 document.getElementById("description-day1").innerHTML = data.list[0].weather[0].main;
 document.getElementById("img-day1").src = "http://openweathermap.org/img/wn/"+data.list[0].weather[0].icon+".png";
@@ -138,7 +161,7 @@ document.getElementById("humidity-day1").innerHTML = data.list[0].main.humidity 
 document.getElementById("sunrise-day1").innerHTML = sunriseHour;
 document.getElementById("sunset-day1").innerHTML = sunsetHour;
 
-let k=8;
+//----Get data for the next days---------
 for(i=1; i<5; i++){
     document.getElementById("temperature-day" + (i+1)).innerHTML = Number(data.list[k].main.temp -273.15).toFixed(0) + "°";
     k = k+8;
@@ -149,15 +172,14 @@ for(i=1; i<5; i++){
     document.getElementById("humidity-day" + (i+1)).innerHTML = data.list[i].main.humidity + "%";
 }
 
+//----Get data for the next hours---------
 for(i=0; i<6; i++){
     document.getElementById("temperature-hour" + (i+1)).innerHTML = Number(data.list[i].main.temp -273.15).toFixed(0) + "°";
     document.getElementById("image-hour" + (i+1)).src = "http://openweathermap.org/img/wn/"+data.list[i].weather[0].icon+".png";   
 }
 
-var currentIcon = data.list[0].weather[0].main;
-var currentDt = data.list[0].dt;
-var currentHour = getHour(currentDt);
-    if(currentHour == 18 || currentHour == 21 || currentHour == 0 || currentHour == 3 || currentHour == 6){
+//----Check the current time if it's day or night---------
+if(currentHour == 18 || currentHour == 21 || currentHour == 0 || currentHour == 3 || currentHour == 6){
     //Getting background for night
         switch (currentIcon){
             case "Rain":
